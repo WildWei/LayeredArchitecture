34b296a8c9def61de5a7f0aed8e4adb43b34bfd5
William <a0937529646@gmail.com>
2020-01-04 14:15:22 +0800

    add start using

--- App.All/WebApplication/Startup.cs
+++ App.All/WebApplication/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CakeMyBlog.DataAccessLayer;
 using CakeMyBlog.DataAccessLayer.Interface;
 using CakeMyBlog.Service;
 using CakeMyBlog.Service.Interface;
